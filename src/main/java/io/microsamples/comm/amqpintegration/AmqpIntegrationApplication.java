c531be5b0b6ab8c82e671a62f45e5e912628323e
Alex Shumilov <ashumilov@pivotal.io>
2018-10-09 22:24:08 -0400

    Experiment with testing.

--- src/main/java/io/microsamples/comm/amqpintegration/AmqpIntegrationApplication.java
+++ src/main/java/io/microsamples/comm/amqpintegration/AmqpIntegrationApplication.java
@@ -2,8 +2,13 @@ package io.microsamples.comm.amqpintegration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.integration.config.EnableIntegration;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.ip.dsl.Tcp;
+import org.springframework.integration.ip.tcp.connection.TcpNetServerConnectionFactory;
 
 @SpringBootApplication
 @ImportResource("classpath*:/spring/integration/tcp-amqp.xml")
@@ -12,4 +17,5 @@ public class AmqpIntegrationApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AmqpIntegrationApplication.class, args);
 	}
+
 }
--- src/test/java/io/microsamples/comm/amqpintegration/AmqpIntegrationApplicationTests.java
+++ src/test/java/io/microsamples/comm/amqpintegration/AmqpIntegrationApplicationTests.java
@@ -1,16 +1,50 @@
 package io.microsamples.comm.amqpintegration;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class AmqpIntegrationApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    @Qualifier("toRabbit")
+    private DirectChannel toRabbit;
+
+    @Autowired
+    private Queue queue;
+
+    @Autowired
+    private RabbitTemplate amqpTemplate;
+
+    @Test
+    @Ignore
+    /**
+     * This will verify if message was posted to the queue.  TCP out section better be removed from tcp-amqp.xml, when running this test.
+     */
+    public void processFlow() {
+        byte[] note = "Hi there".getBytes();
+        org.springframework.messaging.Message<byte[]> toSend = MessageBuilder.withPayload(note).build();
+
+        toRabbit.send(toSend);
+
+        amqpTemplate.setQueue(queue.getName());
+        Message receive = amqpTemplate.receive();
+        assertThat(receive.getBody()).containsExactly(note);
+    }
 
 }
